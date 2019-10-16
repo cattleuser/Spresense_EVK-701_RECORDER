58ff984866ba6309128aa7ccb3f586763ccba29f
Animal Silent Voice Project <Fukawa.Masamoto@technopro.com>
2019-10-16 20:17:10 +0900

    Update cow_log.h

--- sd_acc_press_gps/cow_log.h
+++ sd_acc_press_gps/cow_log.h
@@ -1,8 +1,32 @@
+/*
+MIT License
+
+Copyright (c) 2019 Animal Silent Voice Project
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 /**
  * @file cow_log_sys.ino
  * @author Technopro
- * @brief Save cow acceleration to SD card.
- * @details It is a sketch for recording the behavior of the cow.
+ * @brief Save Animal acceleration and pressure to SD card.
+ * @details It is a sketch for recording the behavior of the Animal.
  */
 
 /**
@@ -12,22 +36,22 @@
 /**
  * @brief Macro definitions
  */
-#define CONFIG_FILE_NAME    "tracker.ini"  /**< Config file name */
-#define CONFIG_FILE_SIZE    4096           /**< Config file size */
-#define INDEX_FILE_NAME    "index.ini"     /**< Index file name */
-#define INDEX_FILE_SIZE    16              /**< Index file size */
-#define STRING_BUFFER_SIZE  128            /**< %String buffer size */
-#define NMEA_BUFFER_SIZE    128            /**< NMEA buffer size */
-#define SENSOR_BUFFER_SIZE  128
-#define OUTPUT_FILENAME_LEN 20             /**< Output file name length. */
-#define DEFAULT_INTERVAL_SEC    1          /**< Default positioning interval in seconds. */
-#define SERIAL_BAUDRATE     115200         /**< Serial baud rate. */
-#define SEPARATOR           0x0A           /**< Separator */
-#define SENSOR_INTERVAL 40                 /* [ms]. */
-#define SENSOR_INTERVAL_WRITE 4000         /* must be a multiple SENSOR_INTERVAL[ms]. */
-#define FILE_INTERVAL 1800000               /* [ms], = 300[s]. */
-#define GPS_INTERVAL 1000                 /* [ms] */
-#define MY_TIMEZONE_IN_SECONDS (9 * 60 * 60) /* JST */
+#define CONFIG_FILE_NAME       "tracker.ini"  /**< Config file name */
+#define CONFIG_FILE_SIZE       4096           /**< Config file size */
+#define INDEX_FILE_NAME        "index.ini"    /**< Index file name */
+#define INDEX_FILE_SIZE        16             /**< Index file size */
+#define STRING_BUFFER_SIZE     128            /**< String buffer size */
+#define NMEA_BUFFER_SIZE       128            /**< NMEA buffer size */
+#define SENSOR_BUFFER_SIZE     128            /**< SENSOR buffer size */
+#define OUTPUT_FILENAME_LEN    20             /**< Output file name length. */
+#define DEFAULT_INTERVAL_SEC   1              /**< Default positioning interval in seconds. */
+#define SERIAL_BAUDRATE        115200         /**< Serial baud rate. */
+#define SEPARATOR              0x0A           /**< Separator */
+#define SENSOR_INTERVAL        40             /**< [ms] */
+#define SENSOR_INTERVAL_WRITE  4000           /**< [ms] */
+#define FILE_INTERVAL          1800000        /**< [ms] */
+#define GPS_INTERVAL           1000           /* [ms] */
+#define MY_TIMEZONE_IN_SECONDS (9 * 60 * 60)  /* JST[s] */
 
 /**
  * @enum LoopState
